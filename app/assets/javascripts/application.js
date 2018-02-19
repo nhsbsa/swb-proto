24cf0d7f13b2b47e7e5f5e42716f029d1f3c9701
helenspires <helen.spires@digital.hmrc.gov.uk>
2018-02-19 11:53:40 +0000

    stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,14 +14,13 @@ $(document).ready(function () {
   // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
   // with role="button" when the space key is pressed.
   GOVUK.shimLinksWithButtonRole.init()
-
   // Show and hide toggled content
   // Where .multiple-choice uses the data-target attribute
   // to toggle hidden content
   var showHideContent = new GOVUK.ShowHideContent()
   showHideContent.init()
-})
-$('.header').click(function(){
+    
+    $('.header').click(function(){
      $(this).toggleClass('expand').nextUntil('tr.header').slideToggle(100);
 });
     
@@ -52,3 +51,17 @@ $(document).ready(function(){
   })
 
 })
+    
+    $(document).ready(function() {
+
+    $('.internal-table tr').click(function() {
+        var href = $(this).find("a").attr("href");
+        if(href) {
+            window.location = href;
+        }
+    });
+
+});
+
+    
+  
